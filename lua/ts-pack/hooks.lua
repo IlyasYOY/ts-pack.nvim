b54620f6fca3036cca3e3cb4a093253cbe9ea1c9
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-05-07 23:00:17 +0300

    feat: add automatic treesitter feature setup

--- lua/ts-pack/hooks.lua
+++ lua/ts-pack/hooks.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local features = require('ts-pack.features')
+
 function M.apply_filetype(parser)
   local data = parser.data
   if not data or data.filetype == nil then
@@ -17,6 +19,7 @@ end
 
 function M.apply(parser)
   M.apply_filetype(parser)
+  features.register(parser)
 end
 
 return M
--- lua/ts-pack/init.lua
+++ lua/ts-pack/init.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+local features = require('ts-pack.features')
 local fs = require('ts-pack.fs')
 local hooks = require('ts-pack.hooks')
 local install = require('ts-pack.install')
@@ -315,6 +316,7 @@ function M.del(names, opts)
     vim.fn.delete(path.query_path(name, opts), 'rf')
     lock.parsers[name] = nil
     active[name] = nil
+    features.unregister(name)
     result[#result + 1] = { name = name, deleted = true }
   end
 
